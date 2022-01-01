8631190e130971aac98c98d24e2fabaca46b931f
George Miao <gm@miao.dev>
2021-12-31 21:45:33 -0500

    feat: SEO tags

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { projects, Category } from "./projects";
 
 import "./main.css";
 
+const preview =
+  "https://imagedelivery.net/b21oeeg7p6hqWEI-IA5xDw/0db4540a-72fb-4f01-6a04-a03ad5bf8400/public";
+
 declare global {
   interface Window {
     projects: Category[];
