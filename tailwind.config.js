a4cf7c288022a101a80e638960394ce1e6a21ed4
George Miao <gm@miao.dev>
2022-01-14 23:39:30 -0500

    perf: use custom theme color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      colors: {
+        red: {
+          700: "#be223a",
+        },
+      },
       fontFamily: {
         sans: ["Josefin-sans", "Avenir"],
       },
