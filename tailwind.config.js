75c7dec3ede523d5f11471e7c98484a82647f9b4
George Miao <gm@miao.dev>
2022-01-14 23:37:24 -0500

    feat: use josephin sans in title

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Josefin-sans", "Avenir"],
+      },
+    },
   },
   plugins: [],
 };
